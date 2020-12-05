4451d491fe2865bc9b73e58d07d31490628b63e9
Bahyeddin Nuri <bahyaddin.nuri@westerops.com>
2020-12-05 20:04:42 +0400

    user login page is ready

--- swe573front/src/App.jsx
+++ swe573front/src/App.jsx
@@ -1,27 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import './App.less';
+
+import Routes from './routes';
+import theme from './theme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes />
+      </Router>
+    </ThemeProvider>
   );
 }
 
